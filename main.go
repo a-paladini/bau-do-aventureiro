ec905d6c098f08684326a57db76e02196354ca22
a-paladini <aline.paladini_yay@hotmail.com>
2025-04-20 14:59:47 -0300

    - Refactor configuration management: load DB and server settings from environment variables using Viper

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/a-paladini/bau-do-aventureiro/api"
 	db "github.com/a-paladini/bau-do-aventureiro/db/sqlc"
+	"github.com/a-paladini/bau-do-aventureiro/db/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/bau_t20?sslmode=disable"
-	serverAddress = "localhost:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
